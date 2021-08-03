34a077e8af8c952945e0dab743dcc7ce2ab469ec
zct1989 <2037630@gmail.com>
2021-08-03 09:07:10 +0000

    Deploying to gh-pages from @ gopowerteam/app.h5-editor@e457f2ec64658525581b6a91272f915bd32b6b72 🚀

--- assets/h5-center.7b096d55.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as a,s as r}from"./vendor.12b0abc3.js";class t extends s{constructor(s){super(),a(this,s,null,null,r,{})}}export{t as default};
---
+++ assets/h5-center.c8e027d6.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as r}from"./vendor.e6f185d8.js";class t extends s{constructor(s){super(),e(this,s,null,null,r,{})}}export{t as default};
