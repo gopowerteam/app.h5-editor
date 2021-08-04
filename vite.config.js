4bb453f32267cf0111ce8d2f6767400da3b07611
zhuchentong <2037630@163.com>
2021-08-04 17:44:05 +0800

    更新CI流程

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
 import svelteSVG from 'vite-plugin-svelte-svg'
-import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,10 +9,6 @@ export default defineConfig({
         svelte(),
         svelteSVG({
             svgoConfig: {} // See https://github.com/svg/svgo#configuration
-        }),
-        EnvironmentPlugin.default({
-            VITE_CLOUDBASE_ENV: process.env.CLOUDBASE_ENV,
-            VITE_CLOUDBASE_REGION: process.env.CLOUDBASE_REGION
         })
     ],
     resolve: {
