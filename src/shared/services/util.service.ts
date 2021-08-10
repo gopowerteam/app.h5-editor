73bd5a72173be5df5fa4201a63985b596d84fd5b
zhuchentong <2037630@163.com>
2021-08-10 09:56:26 +0800

    添加图片时间戳

--- src/shared/services/util.service.ts
+++ src/shared/services/util.service.ts
@@ -1,5 +1,6 @@
 export async function getBase64FromUrl(url) {
-    const data = await fetch(url)
+    const timespan = Math.random().toString(32).slice(2)
+    const data = await fetch(url + `?v=${timespan}`)
     const blob = await data.blob()
     return new Promise((resolve) => {
         const reader = new FileReader()
